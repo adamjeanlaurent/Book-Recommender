e76119227f25dd8b0debfb59e5408eeb41ae0d80
Adam Jean-Laurent <ajean-laurent@my.uri.edu>
2019-08-14 18:37:30 -0400

    can now pick category and get random book

---
+++ app.js
@@ -0,0 +1,48 @@
+const csv = require('csv-parser');
+const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");   
+
+const results = [];
+
+app.get('/', (req,res) => {
+  res.render('index');
+});
+
+app.post('/', (req, res) => {
+  if(req.body.bookType == 'default') {
+    res.redirect('/');
+  }
+  else {
+    fs.createReadStream('Task1/test.csv')
+    .pipe(csv(['amazonIndex', 'filename', 'imageURL', 'title', 'author', 'categoryID', 'category']))
+    // parses CSV, finds all the books with the requested category
+    .on('data', (data) => {
+      if(data.categoryID == '25')
+      results.push(data)
+    })
+    .on('end', () => {
+      // send random book from that category
+      let randomNum = Math.floor((Math.random() * results.length) + 1)
+      let rb = results[randomNum];
+      res.render('book', {title: rb.title, author: rb.author, image: rb.imageURL});
+    });
+  
+   
+  }
+});
+
+app.listen('3000', () => {
+  console.log('listening on port 3000!');
+});
+
+
+
+
+
+
+
